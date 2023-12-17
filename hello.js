248dd2a99a8b341ea89197f91b7fee8fe7e255fd
Chandralekha <chandralekha.murugan@gmail.com>
2023-12-17 17:20:21 -0600

    Now Says Hello World

--- hello.js
+++ hello.js
@@ -5,28 +5,65 @@ to the console (the panel on the right)
 */
 
 console.log('‘Hello, World!');
-console.log('20 + 20');
-console.log('Coding Rocks!')
-console.log("I Love Coding, Coding Rocks!!")
-console.log("Quoth the Raven, Nevermore!!!")
-console.log('20' + '20')
-console.log('Hello, JavaScript.');
-console.log(2001);
-console.log("What","do","commas","do?");
-console.log("Does", "adding",      "space", "matter?");
-console.log('Launch' + 'Code');
-console.log("LaunchCode was founded in", 2013);
-/*  
-Next, PLAY! 
-Modify your code on line 7 to try to accomplish the tasks listed below:
+// console.log('20 + 20');
+// console.log('Coding Rocks!')
+// console.log("I Love Coding, Coding Rocks!!")
+// console.log("Quoth the Raven, Nevermore!!!")
+// console.log('20' + '20')
+// console.log('Hello, JavaScript.');
+// console.log(2001);
+// console.log("What","do","commas","do?");
+// console.log("Does", "adding",      "space", "matter?");
+// console.log('Launch' + 'Code');
+// console.log("LaunchCode was founded in", 2013);
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
-*/
+// console.log("Some Programming Languages:");
+
+// console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+// console.log(typeof "17");
+// console.log(typeof "3.2");
+
+// console.log(Number('3'));
+// console.log(Number('three'));
+// console.log(Number('3 3'));
+// console.log(Number('33'));
+
+//Next, PLAY! 
+//Modify your code on line 7 to try to accomplish the tasks listed below:
+
+  // 1. Change the message that is printed.
+console.log("Hello, JavaScript!!");
+   //2. Figure out what the parentheses do. Will the code work without them?
+   
+   //console.log "Hello" Parantheses is expected and the code will not work without them. 
+   
+   //3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? 
+   //Is there a difference between using a single or a double quote (' vs. ")?
+   
+   //If it's string we need both opening and closing quotation marks with 
+   //either single or double it makes no difference. for number we don't need quotations.
+   
+   //4. Remove the semi-colon, ;.
+
+   //The code works even if we remove the semi-colon, ;.
+   console.log(15)
+
+   //5. Print a number. (Bonus: Print two numbers added together).
+   console.log(35);
+   console.log(40 + 40);
+
+   //6. Print multiple messages one after the other.
+   console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+   //7. Print two messages on the same line.
+   console.log("What","do","commas","do?");
+   console.log("Does", "adding",      "space", "matter?");
+   
+   //8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   console.log("'LaunchCode was founded in'", 2013);
+
+   //9. Other. You choose!
+   console.log("I Love Coding, Coding Rocks!!")
+   console.log('Launch' + 'Code');
+   console.log("Chandra" + "Lekha");
---
+++ let_programmingLanguage.js
@@ -0,0 +1,53 @@
+let programmingLanguage;
+programmingLanguage = "JavaScript";
+console.log(programmingLanguage);
+
+/*let day = "Thursday";
+console.log(day);
+
+day = "Friday";
+console.log(day);
+
+day = 21;
+console.log(day);*/
+
+let day = "Thursday";
+day = 32.5;
+day = 19;
+console.log(day);
+
+//const appName = "Get It Done";
+appName = "Best TODO application Ever!";//
+
+console.log(20 + 20);
+
+console.log(16 - 2 * 5 / 3 + 1);
+console.log(1 + 5 % 3);
+console.log(2 ** 2 ** 3 * 3);
+
+//const input = require('readline-sync');
+
+/*let name = input.question("Enter your name: ");
+console.log("Hello, " + name + "!");//
+
+const input = require('readline-sync');
+
+let num1 = input.question("Enter a number: ");
+let num2 = input.question("Enter another number: ");
+
+console.log(num1 + num2);*/
+
+console.log(2 + 2);
+
+// const input = require('readline-sync');
+// console.log(3);
+// let info;
+// info = input.question("Please enter your age: ");
+// //The user enters 25.
+// // console.log(5);
+// // console.log(typeof info);
+
+const readlineSync = require('readline-sync');
+
+let age = readlineSync.question('Please enter your age: ');
+console.log('Entered age:', age);
